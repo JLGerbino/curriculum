e326989b9370588031098913dac90d50c62adae4
jlgerbino <joselgerbino@gmail.com>
2023-02-26 08:57:26 -0300

    curriculum

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+document.getElementById("contacto").addEventListener("click", function(){       
+    document.getElementById("datos").style.display = "block";    
+})
+document.getElementById("ocultar").addEventListener("click", function(){    
+    document.getElementById("datos").style.display = "none";    
+})
+
+document.getElementById("habilidades").addEventListener("click", function(){        
+    document.getElementById("habil").style.display = "block";    
+})
+document.getElementById("ocultar1").addEventListener("click", function(){
+    document.getElementById("habil").style.display = "none";    
+})
+
+document.getElementById("idiomas").addEventListener("click", function(){        
+    document.getElementById("idiom").style.display = "block";    
+})
+document.getElementById("ocultar2").addEventListener("click", function(){
+    document.getElementById("idiom").style.display = "none";    
+})
