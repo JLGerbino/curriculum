a6d500d168ac4d357d6b79435b659adbcd5d7846
jlgerbino <joselgerbino@gmail.com>
2023-02-27 14:23:35 -0300

    arreglo botones

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-
-
 document.getElementById("contacto").addEventListener("click", function(){       
-    document.getElementById("datos").style.display = "block";    
+    document.getElementById("datos").style.display = "block"; 
+    document.getElementById("contacto").style.display= "none"   
 })
 document.getElementById("ocultar").addEventListener("click", function(){    
-    document.getElementById("datos").style.display = "none";    
+    document.getElementById("datos").style.display = "none";
+    document.getElementById("contacto").style.display = ""    
 })
 
 document.getElementById("habilidades").addEventListener("click", function(){        
-    document.getElementById("habil").style.display = "block";    
+    document.getElementById("habil").style.display = "block"; 
+    document.getElementById("habilidades").style.display = "none";   
 })
 document.getElementById("ocultar1").addEventListener("click", function(){
-    document.getElementById("habil").style.display = "none";    
+    document.getElementById("habil").style.display = "none";
+    document.getElementById("habilidades").style.display = "";    
 })
 
 document.getElementById("idiomas").addEventListener("click", function(){        
-    document.getElementById("idiom").style.display = "block";    
+    document.getElementById("idiom").style.display = "block";
+    document.getElementById("idiomas").style.display = "none";    
 })
 document.getElementById("ocultar2").addEventListener("click", function(){
-    document.getElementById("idiom").style.display = "none";    
+    document.getElementById("idiom").style.display = "none"; 
+    document.getElementById("idiomas").style.display = "";   
 })
+
+//Intente hacer algo mas generico para no repetir codigo pero no pude hacerlo funcionar!!!
+// const botonMostrar = document.querySelectorAll(".boton1");
+// const botonOcultar = document.querySelectorAll(".boton2");
+
+// const mostrar = function (evento) {
+//     console.log("Muestra") 
+//     // document.querySelectorAll(".menu").style.display = "block"; 
+//     // document.querySelectorAll(".boton1").style.display= "none";
+// } 
+
+// const ocultar = function (evento) {
+//     console.log("oculto")
+//     // document.querySelectorAll(".boton1").style.display = "none";
+//     // document.querySelectorAll(".menu").style.display = "" 
+// }
+
+// botonMostrar.forEach ( boton=> {
+//     boton.addEventListener("click", mostrar);
+// });
+
+// botonOcultar.forEach( boton=> {
+//     boton.addEventListener("click", ocultar)
+// })
+
